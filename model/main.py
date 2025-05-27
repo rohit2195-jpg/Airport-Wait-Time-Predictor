06e33db1a149e6ff27fe218ab24198317f6a4f96
rohit2195-jpg <rsattulu@purdue.edu>
2025-05-27 12:09:07 -0400

    added remaining airport

--- model/main.py
+++ model/main.py
@@ -16,18 +16,18 @@ filepath = '../data_past/Awt.cbp.gov_DFW_2024-08-05-2025-14-05.csv'
 list = os.listdir("../data")
 
 
-for file in os.listdir("../data"):
+for file in os.listdir("../data_past"):
     if file.endswith(".csv"):
         filepath = file
-        file = os.path.join("..", "data", file)
+        file = os.path.join("..", "data_past", file)
         data = pd.read_csv(file)
         aircode = filepath[filepath.find('_') + 1:filepath.find('_') + 4]
         train_set = data.sample(frac=0.8, random_state=0)
         test_set = data.drop(train_set.index)
         print(aircode)
 
-        xgb_model(train_set, test_set, airport_to_location, aircode)
-        #checkAccuracy(train_set, test_set, airport_to_location, aircode)
+        #xgb_model(train_set, test_set, airport_to_location, aircode)
+        checkAccuracy(train_set, test_set, airport_to_location, aircode)
 
 #CustomLinearRegression(train_set, test_set, airport_to_location)
 
